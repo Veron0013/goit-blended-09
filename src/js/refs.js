a06937a4094e0cb4d9a21e5ea9aa1b8ea3e35cd5
Mr.Igor <iv_mirsoft@ukr.net>
2025-06-08 13:11:46 +0300

    t1

---
+++ src/js/refs.js
@@ -0,0 +1,25 @@
+
+export function checkValidate(value, type) {
+
+	const returnObj = { state: "", message: "" };
+	let p_OK = "";
+
+	switch (type) {
+		case "string":
+			p_OK = value.trim().length > 3 ? "OK" : "NO";
+			returnObj.state = p_OK;
+			returnObj.message = p_OK === "OK" ? "" : "Enter valid data. More than 3 length";
+			break;
+		case "num":
+			p_OK = Number(value.trim()) > 0 ? "OK" : "NO";
+			returnObj.state = p_OK;
+			returnObj.message = p_OK === "OK" ? "" : "Enter valid data. Number req"
+		default:
+			returnObj.state = "NO";
+			returnObj.message = "Error";
+
+	}
+
+
+	return returnObj;
+}
--- src/main.js
+++ src/main.js
@@ -14,3 +14,84 @@
       <p>Текст</p>
   </li>
 */
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { checkValidate } from "./js/refs";
+
+const _OK = "OK";
+const _NO = "NO";
+
+const type_STRING = "string";
+const type_Num = "num";
+
+const state_OK = "Success";
+const state_ERROR = "Error";
+
+const col_red = "#ef4040";
+const col_green = "#008000";
+
+
+const eventList = document.querySelector("ul.tasks-list");
+
+const formInput = document.querySelector(".header-form");
+//const btnAdd = document.querySelector("header-form-btn");
+const inputTitle = document.querySelector("input[name='taskName']");
+const inputTask = document.querySelector("input[name='taskDescription']");
+
+
+
+function addEvent() {
+  let objVal = {};
+
+  const titleValue = inputTitle.value.trim();
+  const titleDesc = inputTask.value.trim();
+
+  objVal = checkValidate(titleValue, type_STRING);
+
+  if (objVal.state !== _OK) {
+    showNotification(state_ERROR, objVal.message);
+    return;
+  }
+
+  objVal = checkValidate(titleDesc, type_STRING);
+
+  if (objVal.state !== _OK) {
+    showNotification(state_ERROR, objVal.message);
+    return;
+  }
+
+  const newEl =
+    `<li class="task-list-item">
+  <button class="task-list-item-btn">Delete</button>
+  <h3>${titleValue.trim()}</h3>
+  <p>${titleDesc.trim()}</p>
+  </li>`;
+
+  console.log(newEl);
+
+  eventList.insertAdjacentHTML("beforeend", newEl);
+
+  inputTitle.value = "";
+  inputTask.value = "";
+
+  showNotification(state_OK, "Event added", col_green);
+
+}
+
+function showNotification(state, message, type = col_red) {
+  iziToast.show({
+    id: String(state).toLocaleLowerCase(),
+    title: state,
+    message: message,
+    messageColor: 'white',
+    color: type,
+    position: 'bottomCenter',
+  });
+}
+
+
+formInput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addEvent();
+});
